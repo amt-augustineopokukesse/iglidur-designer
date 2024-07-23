674d1229387e289bf7e13604832a694e7f37ed3b
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-23 12:58:02 +0000

    chore: change db configuration module to a file

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from '../database/database.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from '../database/database';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    DatabaseModule,
-  ],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(databaseConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
