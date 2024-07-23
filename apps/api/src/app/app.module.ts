77a724348c1381ce9384816d14834612190d3e67
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-23 11:22:21 +0000

    chore: add database configuration to application module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '../database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      
-    })
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
