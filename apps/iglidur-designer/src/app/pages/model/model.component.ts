9ea7981a99cc9ca7f644753c40694dd9f3ffaf66
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-24 13:33:26 +0000

    feat: add header library

--- apps/iglidur-designer/src/app/pages/model/model.component.ts
+++ apps/iglidur-designer/src/app/pages/model/model.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '@iglidur-designer/header';
 
 @Component({
   selector: 'app-model',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './model.component.html',
   styleUrl: './model.component.css',
 })
