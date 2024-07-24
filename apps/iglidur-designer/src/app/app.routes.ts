783c1bbfd470e98c564e97e0141ee29f59adae36
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-24 10:29:44 +0000

    feat: add model page

--- apps/iglidur-designer/src/app/app.routes.ts
+++ apps/iglidur-designer/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { ModelComponent } from './pages/model/model.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', component: ModelComponent },
+];
---
+++ apps/iglidur-designer/src/app/pages/model/model.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-model',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './model.component.html',
+  styleUrl: './model.component.css',
+})
+export class ModelComponent {}
