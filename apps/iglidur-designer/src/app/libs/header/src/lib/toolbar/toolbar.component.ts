53ff38f6675e9add5e882706e0452b943250dc99
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-25 15:28:38 +0000

    feat: add navigation menu

--- apps/iglidur-designer/src/app/libs/header/src/lib/toolbar/toolbar.component.ts
+++ apps/iglidur-designer/src/app/libs/header/src/lib/toolbar/toolbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'lib-toolbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatTabsModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  links = ['MODEL', 'MATERIAL', 'POST-PROCESSING', 'PRODUCIBILITY'];
+  activeLink = this.links[0];
+}
