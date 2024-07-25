1873572d57db401edd15f3b25e73a57358075a7f
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-25 13:51:38 +0000

    feat: add toolbar files and structure

--- apps/iglidur-designer/src/app/app.component.ts
+++ apps/iglidur-designer/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '@iglidur-designer/header';
+import { HeaderComponent, ToolbarComponent } from '@iglidur-designer/header';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, ToolbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
