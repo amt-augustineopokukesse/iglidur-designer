3649226238ca0efdabe0c9625b8bab4fca099a00
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-29 16:49:03 +0000

    feat: create model page hero

--- apps/iglidur-designer/src/app/pages/model/model.component.ts
+++ apps/iglidur-designer/src/app/pages/model/model.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LangService } from '@iglidur-designer/services';
+// import tools from '../../../assets/images/tools.png';
 
 @Component({
   selector: 'app-model',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './model.component.html',
   styleUrl: './model.component.scss',
 })
-export class ModelComponent {}
+export class ModelComponent {
+  lang!: string;
+  tools = '../../../assets/images/tools.png';
+  constructor(private translate: TranslateService, private langService: LangService) {
+    this.langService.lang$.subscribe((lang) => {
+      this.lang = lang;
+      this.translate.use('model.component.i18n');
+    })
+    
+
+  }
+}
