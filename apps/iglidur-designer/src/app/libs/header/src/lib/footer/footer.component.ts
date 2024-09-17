9dd71c57953fb20c9ef86f416a664c9c4d5a6639
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-09-17 21:06:41 +0000

    feat: footer i18n

--- apps/iglidur-designer/src/app/libs/header/src/lib/footer/footer.component.ts
+++ apps/iglidur-designer/src/app/libs/header/src/lib/footer/footer.component.ts
@@ -1,10 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '@iglidur-designer/services';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'lib-footer',
@@ -16,13 +19,37 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     FormsModule,
     MatButtonModule,
+    TranslateModule,
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
   rating = 0;
   stars = new Array(5);
+  private destroy$ = new Subject<void>();
+  language!: string;
+
+  constructor(
+    private translate: TranslateService,
+    private languageService: LanguageService,
+  ) {}
+
+  ngOnInit(): void {
+    this.languageService.language$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((language) => {
+        this.language = language;
+        this.translate.use('footer.component.i18n');
+      });
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   rate(rating: number) {
     this.rating = rating;
