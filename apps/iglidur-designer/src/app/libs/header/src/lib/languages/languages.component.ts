7615a97ff3691063fdf10807eeddb038136ddb84
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-10-04 12:37:10 +0000

    fix: rebase conflict

--- apps/iglidur-designer/src/app/libs/header/src/lib/languages/languages.component.ts
+++ apps/iglidur-designer/src/app/libs/header/src/lib/languages/languages.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { findFlagUrlByCountryName } from 'country-flags-svg';
 import { LanguageService } from '@iglidur-designer/services';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { flagUrls } from '@iglidur-designer/utils';
 
 @Component({
   selector: 'lib-languages',
@@ -15,13 +16,7 @@ import { LanguageService } from '@iglidur-designer/services';
 export class LanguagesComponent {
   constructor(private languageService: LanguageService) {}
   emitClose = output<boolean>();
-  flagUrls = {
-    uk: findFlagUrlByCountryName('United Kingdom'),
-    us: findFlagUrlByCountryName('United States'),
-    fr: findFlagUrlByCountryName('France'),
-    de: findFlagUrlByCountryName('Germany'),
-    jp: findFlagUrlByCountryName('Japan')
-  };
+  flagUrls = flagUrls;
 
   onClose() {
     this.emitClose.emit(true);
